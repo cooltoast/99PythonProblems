a02b5a0f7a801f6a501e2c39a51f37a767149c5f
cooltoast <mpatel678@gmail.com>
2014-05-10 23:25:11 -0700

    somehow sped up program with 7 less additions

--- highlydivisibletriangularnumber.py
+++ highlydivisibletriangularnumber.py
@@ -7,8 +7,8 @@ def divisors (n):
       divisors = divisors + 1
   return divisors
   
-num = 55
-addnum = 10
+num = 55 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18
+addnum = 18
 
 while (divisors(num) <= 500):
   addnum = addnum + 1
