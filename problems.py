a3e76e77dc4addf9517cd4ee40b5731d8cd75ef9
Markan Patel <cooltoast@users.noreply.github.com>
2015-02-04 22:54:38 -0500

    assert problem 14

--- problems.py
+++ problems.py
@@ -163,7 +163,7 @@ def duplicate (some_list):
     duplicate_list.append(i)
   return duplicate_list  
 
-print duplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+assert(duplicate(range(10)) == [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9])
 
 
 #Problem 15
