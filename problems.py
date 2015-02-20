b72f4c31987b11df8dcadc20db06daca7d657718
cooltoast <mpatel678@gmail.com>
2015-02-19 22:13:34 -0500

    range_list testing ;

--- problems.py
+++ problems.py
@@ -271,12 +271,12 @@ assert(insert_at(a_list, 88, 5) == [0, 45, 1, 15, 2, 88, 3, 4, 5, 6, 7, 8, 9])
 #Problem 22
 print "~~~~~~~~~~~ PROBLEM 22 ~~~~~~~~~~~~~"
 def range_list (lower, upper):
-  range_list = range(lower, upper + 1)
-  return range_list   
+  return range(lower, upper + 1)
 
-print "Range of list from 1 to 10: ", range_list(1, 10)
-print "Range of list from 4 to 30: ", range_list(4, 30)
-print "Range of list from 17 to 56: ", range_list(17, 56)
+assert(range_list(1, 10) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+assert(range_list(4, 30) == [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30])
+assert(range_list(17, 56) == [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56])
+
 
 
 #Problem 23
